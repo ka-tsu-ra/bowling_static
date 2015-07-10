eb5881ef4cb81206e39aa94733dd4000003242c5
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-07-10 19:38:21 +0100

    Add test for max roll scores

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -17,4 +17,20 @@ describe('Bowling', function() {
       expect(bowling.rollArray[0]).toBe(6);
     });
   });
+
+  describe('frameScores', function() {
+    it('adds up the number of fallen pins in two rolls', function() {
+      bowling.roll(6);
+      bowling.roll(3);
+      expect(bowling.frameScores[0]).toBe(9);
+    });
+  });
+
+  describe('rolling', function() {
+    it('does not allow rolls over maximum possible pins', function() {
+      bowling.roll(8);
+      bowling.roll(6);
+      expect(bowling.rollArray.length).toBe(1);
+    });
+  });
 });
--- src/Bowling.js
+++ src/Bowling.js
@@ -1,13 +1,16 @@
 function Bowling() {
   this.scoreTotal = 0;
+  this.frameScores = [];
   this.rollArray = [];
 };
 
-// Bowling.prototype.roll = function() {
-//   var x = Math.floor((Math.random() * 11));
-//   this.scoreTotal +=x;
-// };
+rollCount = 0
 
 Bowling.prototype.roll = function(num) {
+  rollCount ++;
   this.rollArray.push(num);
+  if(rollCount % 2 == 0){
+    var frameScore = (this.rollArray[rollCount-2]) +  (this.rollArray[rollCount-1]);
+    this.frameScores.push(frameScore);
+  };
 };
