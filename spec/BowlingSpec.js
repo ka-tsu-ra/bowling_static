be55a431c38fd21d55e18d52be5aa1ba68452274
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-07-10 18:21:09 +0100

    Fix test syntax. Add zero starting total to object

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -1,10 +1,13 @@
 describe('Bowling', function() {
   var bowling;
 
+  beforeEach(function() {
+    bowling = new Bowling();
+  });
+
   describe('count', function() {
     it('displays scores as zero at the start', function() {
-      bowling = new Bowling();
-      expect(scoreTotal()).toBe(0);
+      expect(bowling.scoreTotal).toBe(0);
     });
   });
 });
---
+++ src/Bowling.js
@@ -0,0 +1,3 @@
+function Bowling() {
+  this.scoreTotal = 0;
+}
