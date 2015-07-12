9bee751e88cbbe671137e3e256e5b78bbc78b067
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-07-12 15:36:19 +0100

    Added basic structure in sinatra file. Added basic html structure.

--- lib/bowling_web.rb
+++ lib/bowling_web.rb
@@ -1,10 +1,19 @@
 require 'sinatra/base'
 
 class BowlingWeb < Sinatra::Base
+
+  enable :sessions
+
+  set :views, proc { File.join(root, '..', 'views') }
+  set :public_folder, proc { File.join(root, '..', 'public') }
+
   get '/' do
-    'Hello BowlingWeb!'
+    redirect '/start'
   end
 
+  get '/start' do
+    erb :'start'
+  end
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
